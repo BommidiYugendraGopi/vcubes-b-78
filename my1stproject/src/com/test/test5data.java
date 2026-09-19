9b8dcee74b5e74730a83fe4aa12360e843caa7b2
BommidiYugendraGopi <yugendragopi@gmail.com>
2026-09-19 10:02:22 +0530

    5test

---
+++ my1stproject/src/com/test/test5data.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class test5data {
+	byte d;
+	public static void main() {
+		byte f;
+		test5data t =new test5data();
+		System.out.println(t.d);	
+		System.out.println(f);}
+
+}
